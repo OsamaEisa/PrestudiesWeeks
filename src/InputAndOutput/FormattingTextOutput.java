f2327b9e623491d0ed1b64fe7a60af46c701a6b1
OsamaEisa <engosama46@yahoo.com>
2024-04-12 19:56:20 +0200

    to print a ship using Ascii

---
+++ src/InputAndOutput/FormattingTextOutput.java
@@ -0,0 +1,13 @@
+package InputAndOutput;
+
+public class FormattingTextOutput {
+    public static void main(String[] args) {
+
+        // printing a ship using the Ascii art technique
+        System.out.println("                 __/___            ");
+        System.out.println("           _____/______|           ");
+        System.out.println("    ______/_____\\______\\______");
+        System.out.println("    \\             < < <       |   ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+}
