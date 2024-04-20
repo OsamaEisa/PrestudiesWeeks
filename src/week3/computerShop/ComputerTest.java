49a58e347bda5b97a578df24da2291b93b0215f1
OsamaEisa <engosama46@yahoo.com>
2024-04-21 00:02:11 +0200

    creating a getComputers method just to wrap the code of creating computers list

--- src/week3/computerShop/ComputerTest.java
+++ src/week3/computerShop/ComputerTest.java
@@ -3,6 +3,7 @@ package week3.computerShop;
 import java.util.ArrayList;
 
 public class ComputerTest {
+
   public static void main(String[] args) {
 
     Computer computer = new Computer("HP", 3201, 8,
@@ -11,20 +12,32 @@ public class ComputerTest {
     computer.setRamGB(8 * 2); // doubling the RAM
     System.out.println(computer.toString()); // printing the Computer again
 
-    // creating 5 Computers to test the calculate profit method
+    // calling the method we created to get all Computers
+    ArrayList<Computer> computers = getComputers(); // check the code below the main method
 
+
+    // passing the computers list created to the constructor of the computerShop
+    ComputerShop computerShop = new ComputerShop(computers);
+    // calling the calculateProfit method to get the total profit
+    System.out.printf("The potential profit if all computers are sold would be: %.2f EUR", computerShop.calculateProfit());
+
+  }
+
+  // a method to create the computers and return array list of computers objects
+  private static ArrayList<Computer> getComputers() {
+
+    // creating 5 Computers to test the calculate profit method
     Computer computer1 = new Computer("Dell", 2500, 16,
             1000, (byte) 6, 17.3f, "laptop", true, 650, 750);
-Computer computer2 = new Computer("Lenovo", 1800, 4,
+    Computer computer2 = new Computer("Lenovo", 1800, 4,
             500, (byte) 2, 14.0f, "laptop", false, 600, 700);
-Computer computer3 = new Computer("Apple", 3500, 32,
+    Computer computer3 = new Computer("Apple", 3500, 32,
             2000, (byte) 4, 21.5f, "desktop", true, 800, 900);
-Computer computer4 = new Computer("Asus", 3200, 8,
+    Computer computer4 = new Computer("Asus", 3200, 8,
             1000, (byte) 6, 27.0f, "desktop", false, 700, 750);
-Computer computer5 = new Computer("Acer", 3000, 16,
+    Computer computer5 = new Computer("Acer", 3000, 16,
             1500, (byte) 4, 23.8f, "all-in-one", true, 750, 800);
-
-// An Array list to take all 5 computers
+    // An Array list to take all 5 computers
     ArrayList<Computer> computers = new ArrayList<>();
     computers.add(computer1);
     computers.add(computer2);
@@ -32,9 +45,6 @@ Computer computer5 = new Computer("Acer", 3000, 16,
     computers.add(computer4);
     computers.add(computer5);
 
-    // passing the computers list created to the constructor of the computerShop
-    ComputerShop computerShop = new ComputerShop(computers);
-    System.out.printf("The potential profit if all computers are sold would be: %.2f EUR", computerShop.calculateProfit());
-
+    return computers;
   }
 }
